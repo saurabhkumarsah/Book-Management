f541e0d7c2cfb2cc23769d8038c36cee14bb157e
Saurabh <saurabhsahofficial@gmail.com>
2023-06-22 21:23:54 +0530

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import userModel from "../models/userModel.js"
+import { isValidEmail, isValidMN, isValidPass, isValidTitle } from '../util/validator.js'
 
 
 export const createUser = async (req, res) => {
@@ -7,23 +8,26 @@ export const createUser = async (req, res) => {
         let { title, name, phone, email, password } = req.body
 
         if (!title) return res.status(400).json({ status: false, message: "Title is missing" })
-        if (title !== "Mr" && title !== "Mrs" && title !== "Miss") return res.status(400).json({ status: false, message: "Title should be Mr/Mrs/Miss" })
+        if (!isValidTitle(title)) return res.status(400).json({ status: false, message: "Title should be Mr/Mrs/Miss" })
 
         if (!name) return res.status(400).json({ status: false, message: "Name is missing" })
 
         if (!phone) return res.status(400).json({ status: false, message: "Phone Number is missing" })
+        if (!isValidMN(phone)) return res.status(400).json({ status: false, message: "Phone Number is not valid" })
         const dbPhone = await userModel.findOne({ phone: phone })
         if (dbPhone) return res.status(400).json({ status: false, message: "Phone Number is already exist" })
 
         if (!email) return res.status(400).json({ status: false, message: "Email is missing" })
+        if (!isValidEmail(email)) return res.status(400).json({ status: false, message: "Email is not valid" })
         const dbEmail = await userModel.findOne({ email: email })
         if (dbEmail) return res.status(400).json({ status: false, message: "Email is already exist" })
 
         if (!password) return res.status(400).json({ status: false, message: "Password is missing" })
-        if (password.length < 8 || password.length > 15) return res.status(400).json({ status: false, message: "Invalid Password" })
+        if (!isValidPass(password)) return res.status(400).json({ status: false, message: "Invalid Password" })
 
         const saveData = await userModel.create(req.body)
         return res.status(201).json({ status: true, data: saveData })
+
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message })
     }
@@ -33,6 +37,7 @@ export const createUser = async (req, res) => {
 
 export const userLogin = async (req, res) => {
     try {
+
         let { SECRET_KEY } = process.env
         let { email, password } = req.body
 
@@ -43,8 +48,9 @@ export const userLogin = async (req, res) => {
         if (!password) return res.status(400).json({ status: false, message: "Password is missing" })
         if (userData.password !== password) return res.status(400).json({ status: false, message: "Invalid Password" })
 
-        const token = jwt.sign({ id: userData._id.toString() }, SECRET_KEY, { expiresIn: '1h' })
-        return res.status(201).json({ status: true, data: token })
+        const token = jwt.sign({ id: userData._id.toString() }, SECRET_KEY, { expiresIn: '24h' })
+        return res.status(201).json({ status: true, data: { token: token } })
+
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message })
     }
--- src/util/validator.js
+++ src/util/validator.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import Validator from "email-validator"
 
 export const isId = (id) => {
     if (mongoose.isValidObjectId(id)) return true
@@ -10,6 +11,39 @@ export const isRating = (num) => {
         return true
     }
     else {
+        return false
+    }
+}
+
+export const isValidTitle = (str) => {
+    if (str == "Mr" || str == "Mrs" || str == "Miss") {
+        return true
+    } else {
+        return false
+    }
+}
+
+export const isValidEmail = (email) => {
+    if (Validator.validate(email)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+export const isValidPass = (str) => {
+    let l = str.length
+    if (l >= 8 && l <= 15) {
+        return true
+    } else {
+        return false
+    }
+}
+export const isValidMN = (str) => {
+    let l = str.length
+    if (l === 10) {
+        return true
+    } else {
         return false
     }
 }
