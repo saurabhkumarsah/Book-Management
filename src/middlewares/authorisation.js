4594ab1291cc6c95e9d61973e88f98dccf51d3c8
Saurabh <saurabhsahofficial@gmail.com>
2023-07-06 19:06:06 +0530

    format code

--- src/middlewares/authorisation.js
+++ src/middlewares/authorisation.js
@@ -2,22 +2,24 @@ import jwt from "jsonwebtoken";
 
 export const auth = async (req, res, next) => {
     try {
-        const { SECRET_KEY } = process.env
-        const token = req.headers['x-api-key']
-        if (!token) return res.status(401).json({ status: false, message: "Token is missing" })
+        const { SECRET_KEY } = process.env;
+        const token = req.headers["x-api-key"];
+        if (!token)
+            return res
+                .status(401)
+                .json({ status: false, message: "Token is missing" });
 
         try {
-
-            const decoded = jwt.verify(token, SECRET_KEY)
-            req.decoded = decoded.id
-
+            const decoded = jwt.verify(token, SECRET_KEY);
+            req.decoded = decoded.id;
         } catch (error) {
-            return res.status(403).json({ status: false, message: "Unauthorization" })
+            return res
+                .status(403)
+                .json({ status: false, message: "Unauthorization" });
         }
 
-        next()
-
+        next();
     } catch (error) {
-        return res.status(500).json({ status: false, message: error.message })
+        return res.status(500).json({ status: false, message: error.message });
     }
-}
+};
